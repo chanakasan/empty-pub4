afb71ef5483da70bdc282686d33a9b2b02b03ab3
Chanaka Sandaruwan <chanakasan@gmail.com>
2016-10-18 14:56:47 +0530

    render Hello component

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -3,3 +3,12 @@ console.log('frontend entry loaded!')
 _.times(3, function(i) {
     console.log(`Hello from Lodash ${i+1}`)
 })
+
+
+import ReactDOM from 'react-dom'
+import Hello from './components/Hello'
+
+$(function () {
+  console.log('rendering react component')
+  ReactDOM.render(<Hello />, document.getElementById('frontend_app'))
+})
